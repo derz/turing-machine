61035527c225c2d5376c94c2c5d302e3cbc0dc8d
Dario Merz <dme@hinderlingvolkart.com>
2017-05-02 13:50:03 +0200

    updates example

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
 const TuringMachine = require('./TuringMachine');
 
-const t = new TuringMachine("0000100", "", "");
+(function(num) {
+	const t = new TuringMachine(num, "", "");
 
-t.run(true, function() {
-	const tapes = t.getTapes();
+	t.run(true, function() {
+		const tapes = t.getTapes();
 
-	console.log(
-		'\n\nResult: ' +
-		tapes[tapes.length - 1].getTape().join('').match(/0/g).length +
-		'\n\n'
-	);
-});
+		console.log(
+			'\n\nResult: ' +
+			tapes[tapes.length - 1].getTape().join('').match(/0/g).length +
+			'\n\n'
+		);
+	});
+}('00000100'));
